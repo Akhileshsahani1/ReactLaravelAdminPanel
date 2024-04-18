5d2af10073aaace232c91402dbc95c77b6207719
unknown <sakhilesh@n2rtechnologies.in>
2024-04-18 19:03:28 +0530

    fix-nav

--- resources/js/components/admin/layouts/Navbar.jsx
+++ resources/js/components/admin/layouts/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import avtar1 from '../../../../../public/assets/images/users/avatar-1.jpg'
 function Navbar() {
     return (
             <div className="navbar-custom">
                 <ul className="list-unstyled topbar-menu float-end mb-0">
                     <li className="dropdown notification-list">
-                        <NavLink
+                        <Link
                             className="nav-Link dropdown-toggle nav-user arrow-none me-0"
                             data-bs-toggle="dropdown"
                             to="#"
@@ -25,20 +25,20 @@ function Navbar() {
                                 <span className="account-user-name">Dominic Keller</span>
                                 <span className="account-position">Founder</span>
                             </span>
-                        </NavLink>
+                        </Link>
                         <div className="dropdown-menu dropdown-menu-end dropdown-menu-animated topbar-dropdown-menu profile-dropdown">
                         
                             <div className=" dropdown-header noti-title">
                                 <h6 className="text-overflow m-0">Welcome !</h6>
                             </div>
-                            <NavLink to="javascript:void(0);" className="dropdown-item notify-item">
+                            <Link to="javascript:void(0);" className="dropdown-item notify-item">
                                 <i className="mdi mdi-account-circle me-1" />
                                 <span>My Account</span>
-                            </NavLink>
-                            <NavLink to="javascript:void(0);" className="dropdown-item notify-item">
+                            </Link>
+                            <Link to="javascript:void(0);" className="dropdown-item notify-item">
                                 <i className="mdi mdi-logout me-1" />
                                 <span>Logout</span>
-                            </NavLink>
+                            </Link>
                         </div>
                     </li>
                 </ul>
--- resources/js/components/admin/layouts/Sidebar.jsx
+++ resources/js/components/admin/layouts/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import logo from '../../../../../public/assets/images/logo.png';
 import logo_sm from '../../../../../public/assets/images/logo_sm.png';
 import logo_dark from '../../../../../public/assets/images/logo-dark.png';
@@ -8,29 +8,29 @@ import logo_sm_dark from '../../../../../public/assets/images/logo_sm_dark.png';
 function Sidebar() {
     return (
         <div className="leftside-menu">
-            <NavLink to="index.html" className="logo text-center logo-light">
+            <Link to="index.html" className="logo text-center logo-light">
                 <span className="logo-lg">
                     <img src={logo} alt="" height={16} />
                 </span>
                 <span className="logo-sm">
                     <img src={logo_sm} alt="" height={16} />
                 </span>
-            </NavLink>
-            <NavLink to="index.html" className="logo text-center logo-dark">
+            </Link>
+            <Link to="index.html" className="logo text-center logo-dark">
                 <span className="logo-lg">
                     <img src={logo_dark} alt="" height={16} />
                 </span>
                 <span className="logo-sm">
                     <img src={logo_sm_dark} alt="" height={16} />
                 </span>
-            </NavLink>
+            </Link>
             <div className="h-100" id="leftside-menu-container" data-simplebar="">
                 <ul className="side-nav">
                     <li className="side-nav-item">
-                        <a to="/dashboard" class="side-nav-link">
+                        <Link to="/dashboard" class="side-nav-link nav-link">
                             <i class="uil-home-alt"></i>
                             <span> Dashboard </span>
-                        </a>
+                        </Link>
                     </li>
                 </ul>
                 <div className="clearfix" />
